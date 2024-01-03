373ba2188029e7c3ba97154d5cf6d200060dae16
lisa367 <75017410+lisa367@users.noreply.github.com>
2024-01-03 04:56:26 +0100

    cleaned out files

--- bruteforce.py
+++ bruteforce.py
@@ -14,6 +14,8 @@ raw_dict = {}
 def clean_data(string):
     splitted_str = string.split(";")
     sliced_list = splitted_str[:3]
+    sliced_list[1] = int(float(sliced_list[1]))
+    sliced_list[2] = float(sliced_list[2])
     return sliced_list
     
 
@@ -30,18 +32,16 @@ def import_data(file):
             data = tuple(new_row)
             raw_data.append(data)
             raw_dict[data[0]] = {
-                "price": int(data[1]),
-                "profit": float(data[2].replace(",", ".")),
+                "price": data[1],
+                "profit": data[2],
             }
+
     return raw_data
 
 # Filtrer les combinaisons dont le prix est inferieur ou egal a 500 euros
 def price_within_budget(combination):
-    # print("Combination :\n", combination)
     combination_profits = [raw_dict[share]["price"] for share in list(combination)]
-    #print("Prices : ", combination_profits)
     total_profit = sum(combination_profits)
-    #print("Total : ", total_profit)
     return total_profit <= BUDGET_MAX
 
 
@@ -69,8 +69,8 @@ def all_possible_combinations(shares_list):
 
 # Ordonner les combinaisons selon leur rendement, puis selon leur prix
 def sort_combinations(combinations_list):
-    # combination = (shares, prix, profit)
-    # combinations_list = list of all combinations --> [(), (), (), ...]
+    """ # combination = (shares, prix, profit)
+    # combinations_list = list of all combinations --> [(), (), (), ...] """
     sort_by_profit = sorted(combinations_list, key=itemgetter(2))
     #print(sort_by_profit)
     # sort_by_price = sorted(sort_by_profit, key=itemgetter(1), reverse=True)
@@ -79,57 +79,8 @@ def sort_combinations(combinations_list):
 
 if __name__ == "__main__":
     raw_shares_data = import_data(file_to_import)
-    # print(raw_shares_data)
-    # print("Raw list : \n", raw_data)
-    # print("Raw dict :\n", raw_dict)
-
     all_portfolios_within_budget = all_possible_combinations(raw_shares_data)
-    # print(all_portfolios_within_budget[:30])
-
     sorted_portfolios = sort_combinations(all_portfolios_within_budget)
-
-    #length_portfolio = len(sorted_portfolios)
-    #print(length_portfolio)
-    # Returns 813346 OMG !
-
     print(sorted_portfolios[-1])
     # Returns (('Action-4', 'Action-5', 'Action-6', 'Action-8', 'Action-10', 'Action-11', 'Action-13', 'Action-16', 'Action-17', 'Action-19', 'Action-20'), 500, 598.8000000000001)
 
-    # print(pd.Series(sorted_portfolios[-10:]))
-
-
-
-""" 
-
-# Filtrer les combinaisons dont le prix est inferieur ou egal a 500 euros
-def filter_price_under_500(combinations_list):
-    # combination = (shares, prix, profit)
-    # combinations_list = list of all combinations --> [(), (), (), ...]
-    all_combinations_under_500 = [
-        combination for combination in combinations_list if combination[1] <= BUDGET_MAX
-    ]
-    return all_combinations_under_500
-
-#portfolios_under_500 = filter_price_under_500(all_portfolios_combinations)
-
-# Determiner toutes les combinaisons possibles
-def all_possible_combinations(shares_list):
-    all_shares_names = [share[0] for share in shares_list]
-    print("Shares names : ", all_shares_names)
-    all_shares_combinations = [
-        list(combinations(all_shares_names, i)) for i in range(len(shares_list))
-    ]
-    # print("All combinations : ", all_shares_combinaisons)
-    combinations_full_data = []
-    for combination in all_shares_combinations:
-        all_prices = [raw_dict[share["price"]] for share in combination]
-        all_profits = [raw_dict[share["profit"]] for share in combination]
-        total_price = sum(all_prices)
-        total_profit = sum(all_profits)
-
-        full_data = (combination, total_price, total_profit)
-        combinations_full_data.append(full_data)
-
-    return combinations_full_data
-
-"""
--- optimized.py
+++ optimized.py
@@ -11,25 +11,10 @@ prices = []
 profits = []
 
 
-def clean_data1(string):
-    splitted_str = string.split(";")
-    #print(splitted_str)
-    sliced_list = splitted_str[:3]
-    #print(sliced_list)
-    
-    sliced_list[1] = int(sliced_list[1].replace(",", "."))
-    sliced_list[2] = round(float(sliced_list[2].replace(",", ".")), 1)
-   
-    """ sliced_list[1] = int(float(sliced_list[1]))
-    sliced_list[2] = float(sliced_list[2]) """
-    return sliced_list
 
-
-def clean_data2(string):
+def clean_data(string):
     splitted_str = string.split(";")
-    #print(splitted_str)
     sliced_list = splitted_str[:3]
-    #print(sliced_list)
     """ 
     sliced_list[1] = int(sliced_list[1].replace(",", "."))
     sliced_list[2] = round(float(sliced_list[2].replace(",", ".")), 1)
@@ -39,14 +24,11 @@ def clean_data2(string):
     return sliced_list
     
 
-# Import et calcul des benefices
 def import_data(file):
     with open(file, newline="") as file:
         dataset = csv.reader(file, delimiter=" ", quotechar="|")
-        # print(list(dataset))
         for row in list(dataset)[1:]:
-            new_row = clean_data2(row[0])
-            # print(new_row)
+            new_row = clean_data(row[0])
             data = tuple(new_row)
             raw_data.append(data)
             if data[1] > 0:
@@ -59,69 +41,42 @@ def import_data(file):
 
 def knapsack_algorithm(n):
     matrix = [[0 for j in range(BUDGET_MAX+1)] for i in range(n+1)]
-    print(len(matrix[0]),len(matrix))
-    # print("(i,j): ({i}{j})".format(i,j))
-    top_i, top_j = 0, 0
 
     for i in range(n+1):
         for j in range(BUDGET_MAX+1):
-            #print(i, j)
             if i == 0 or j == 0:
-                #matrix[i][j] = 0
                 continue
             elif prices[i-1] <= j :
-                #print()
                 matrix[i][j] = max(profits[i-1] + matrix[i-1][j-prices[i-1]], matrix[i-1][j])
-                top_i, top_j = i, j
-                #print(f"profits: {profits[i-1]}; j-(i-1): {j} - {prices[i-1]} ")
-                #print(f"(i,j): ({top_i}, {top_j})")
                 # Arg1 = profit of share[i] + profit of element at position in the row above at column [current buget - current price] (j-prices(i))
                 # Arg2 = the data in row above in the matrix ([i-1]), on the same column j
                 # selects the max value between Arg1 and Arg2
             else:
                 matrix[i][j] = matrix[i-1][j]
-                top_i, top_j = i, j
-                #print(f"profits: {profits[i-1]}; j-(i-1): {j} - {prices[i-1]} ")
-                #print(f"(i,j): ({top_i}, {top_j})")
-        #top_i, top_j = i, j
-        #print(f"profits: {profits[i-1]}; j-(i-1): {j-prices[i-1]} ")
-        #print(len(matrix[i]))
         #print(matrix[i])
 
-    # max_profit_within_budget = matrix[n][BUDGET_MAX]
-    
+    estimated_cost = 0.0
     best_portfolio = []
     i = n
     j = BUDGET_MAX
     while (i > 0 and j > 0):
-        #print(i, j)
-        #print(shares)
+        """ #print(i, j)
+        #print(shares) """
         if matrix[i][j] == matrix[i-1][j-prices[i-1]] + profits[i-1]:
-            #print(i, j)
             best_portfolio.append(shares[i-1])
-            #print(best_portfolio)
+            estimated_cost += prices[i-1]
             j -= prices[i-1]
-            #print(j)
         i -= 1
-    print(f"Best portfolio : {best_portfolio}")
-
 
     max_profit_within_budget = matrix[-1][-1]
-    #best_portfolio = output_best_portfolio(matrix=matrix, n=n, shares_list=shares)
-    #print(best_portfolio)
 
-    return max_profit_within_budget, best_portfolio
+    return max_profit_within_budget, best_portfolio, estimated_cost
 
 
 
 if __name__ == "__main__":
-    import_data(file_to_import)
-    #print(raw_data)
-    #print(shares)
-    print("Prices:", len(prices))
-    print("Profits:", len(profits))
-    print("Shares:", len(shares))
-    
-    best_profit, best_portfolio = knapsack_algorithm(n=len(shares))
+    import_data(file_to_import)    
+    best_profit, best_portfolio, estimated_cost = knapsack_algorithm(n=len(shares))
     print(f"Best portfolio : {best_portfolio}")
+    print(f"Estimated cost : {estimated_cost}")
     print(f"Best profit : {best_profit}")
